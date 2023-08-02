527dae091fb2c4d1f6c0ab53f83bc9c078a9a4fb
Mohith kumar <murugan28aug@gmail.com>
2023-08-03 02:04:27 +0530

    made gpt more good

--- main.py
+++ main.py
@@ -58,6 +58,7 @@
 
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from llama_index import GPTSimpleVectorIndex,ServiceContext,LLMPredictor,PromptHelper
 from langchain import OpenAI
 from pydantic import BaseModel
@@ -65,6 +66,17 @@ from decouple import config
 import os
 
 app = FastAPI()
+## Set allowed origins
+origins = [config('ALLOWED_ORIGINS')]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load OpenAI API key from the environment variable
 OPENAI_API_KEY = config('OPENAI_API_KEY')
