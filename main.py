fbf79d22c6ce7414fcb69f6e404b480c1e6bccc4
Mohith kumar <murugan28aug@gmail.com>
2023-08-03 01:55:25 +0530

    made gpt more good

--- main.py
+++ main.py
@@ -1,60 +1,7 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from llama_index import GPTSimpleVectorIndex
-from langchain import OpenAI
-from pydantic import BaseModel
-from decouple import config
-import os
-
-app = FastAPI()
-
-
-## Set allowed origins
-origins = [config('ALLOWED_ORIGINS')]
-
-# Add CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Load OpenAI API key from the environment variable
-OPENAI_API_KEY = config('OPENAI_API_KEY')
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
- 
-# Load the index only once, outside the view, for better performance
-index = GPTSimpleVectorIndex.load_from_disk('index.json')
-
-
-
-
-class QuestionRequest(BaseModel):
-    question: str
-
-
-@app.post("/chat/")
-def chat_api(question_req: QuestionRequest):
-    query = question_req.question
-    # print('Answer like a person,'+query)
-    response = index.query('Answer like you are a person,    '+query)
-    # response_from_ai = openai_client.get_ai_response(
-    #     query)  # Call the OpenAI API for additional response
-    return {"response": response.response}
- 
-
-
-
-
-
-
-
-
 # from fastapi import FastAPI
-# from llama_index import GPTSimpleVectorIndex,ServiceContext,LLMPredictor,PromptHelper
+# from fastapi.middleware.cors import CORSMiddleware
+
+# from llama_index import GPTSimpleVectorIndex
 # from langchain import OpenAI
 # from pydantic import BaseModel
 # from decouple import config
@@ -62,29 +9,25 @@ def chat_api(question_req: QuestionRequest):
 
 # app = FastAPI()
 
+
+# ## Set allowed origins
+# origins = [config('ALLOWED_ORIGINS')]
+
+# # Add CORS middleware
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 # # Load OpenAI API key from the environment variable
 # OPENAI_API_KEY = config('OPENAI_API_KEY')
 # os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
  
 # # Load the index only once, outside the view, for better performance
-
-# # set maximum input size
-# max_input_size = 4096
-# # set number of output tokens
-# num_outputs = 2000
-# # set maximum chunk overlap
-# max_chunk_overlap = 20
-# # set chunk size limit
-# chunk_size_limit = 600 
-
-# # define prompt helper
-# prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
-# # define LLM
-# llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
-
-# service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
-# index = GPTSimpleVectorIndex.load_from_disk('index.json',service_context=service_context)
+# index = GPTSimpleVectorIndex.load_from_disk('index.json')
 
 
 
@@ -96,10 +39,71 @@ def chat_api(question_req: QuestionRequest):
 # @app.post("/chat/")
 # def chat_api(question_req: QuestionRequest):
 #     query = question_req.question
-#     response = index.query(query)
+#     # print('Answer like a person,'+query)
+
+
+
+    
+#     response = index.query('Answer like you are thje person answering the asked question,       question:'+query)
 #     # response_from_ai = openai_client.get_ai_response(
 #     #     query)  # Call the OpenAI API for additional response
 #     return {"response": response.response}
+ 
+
+
+
+
+
+
+
+
+from fastapi import FastAPI
+from llama_index import GPTSimpleVectorIndex,ServiceContext,LLMPredictor,PromptHelper
+from langchain import OpenAI
+from pydantic import BaseModel
+from decouple import config
+import os
+
+app = FastAPI()
+
+# Load OpenAI API key from the environment variable
+OPENAI_API_KEY = config('OPENAI_API_KEY')
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+ 
+# Load the index only once, outside the view, for better performance
+
+# set maximum input size
+max_input_size = 4096
+# set number of output tokens
+num_outputs = 1000
+# set maximum chunk overlap
+max_chunk_overlap = 20
+# set chunk size limit
+chunk_size_limit = 600 
+
+# define prompt helper
+prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+# define LLM
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.1, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+index = GPTSimpleVectorIndex.load_from_disk('index.json',service_context=service_context)
+
+
+
+
+class QuestionRequest(BaseModel):
+    question: str
+
+
+@app.post("/chat/")
+def chat_api(question_req: QuestionRequest):
+    query = question_req.question
+    response = index.query('Answer like you are thje person answering the asked question,       question:'+query)
+    # response_from_ai = openai_client.get_ai_response(
+    #     query)  # Call the OpenAI API for additional response
+    return {"response": response.response}
  
 
 
